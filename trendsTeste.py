b31248bd3491d10b7c5a0a8c938a8d0455f7496f
Joao20037 <joaolucas.moraes.oliveira@gmail.com>
2024-05-09 20:03:06 -0300

    Novo pythonEnv e trendsTeste com pandas

--- trendsTeste.py
+++ trendsTeste.py
@@ -1,15 +1,16 @@
 from serpapi import GoogleSearch
-
+import pandas as pd
 params = {
   "engine": "google_trends",
-  "q": "coffee,milk,bread,pasta,steak",
+  "q": "madonnna",
   "data_type": "TIMESERIES",
   "api_key": "4d73f4242018e9905d140bd94243ec92fc7c84b6ace1965607357bff0748a011"
 }
 
 search = GoogleSearch(params)
 results = search.get_dict()
-interest_over_time = results["interest_over_time"]
+interest_over_time = results
 
-
-print(results)
+df = pd.DataFrame(results["interest_over_time"]["timeline_data"])
+print(df)
+# print(json.dumps(results, indent=4))
