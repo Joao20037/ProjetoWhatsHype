6f292019f9c0c4dcc7ee4e02d4d28766f1272634
Iago Martins <imaartins98@gmail.com>
2024-06-21 13:17:24 -0300

    site

--- site/dash_integration.js
+++ site/dash_integration.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const dashContainer = document.getElementById('dash-container');
     const iframe = document.createElement('iframe');
-    iframe.src = 'http://127.0.0.1:8050/';
+    iframe.src = 'http://127.0.0.1:8051/';
     iframe.style.width = '100%';
     iframe.style.height = '100%';
     iframe.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.1)';
