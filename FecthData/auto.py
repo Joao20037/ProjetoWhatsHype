c1c79b5fc21028a15a4b6f10d3536cee2d4f367d
Joao20037 <joaolucas.moraes.oliveira@gmail.com>
2024-06-21 14:26:36 -0300

    Organizacao por diretorio

---
+++ FecthData/auto.py
@@ -0,0 +1,20 @@
+import subprocess
+
+caminho = r'C:\Users\T-GAMER\Desktop\ProjetoWhatsHype/FecthData'
+
+try:
+    # executar o script youtubePesquisaID.py
+    processo_pesquisa_id = subprocess.Popen(['python', caminho + '\\youtubePesquisaID.py'])
+    # esperar até que o processo do youtubePesquisaID.py seja encerrado
+    processo_pesquisa_id.wait()
+    print("Script youtubePesquisaID.py executado com sucesso!")
+
+    # Executar o script youtubePopular.py
+    processo_popular = subprocess.Popen(['python', caminho + '\\youtubePopular.py'])
+    print("Script youtubePopular.py executado com sucesso!")
+    input("Pressione Enter para fechar esta janela...")
+    
+except Exception as e:
+    print("Ocorreu um erro durante a execução dos scripts:")
+    print(e)
+    input("Pressione Enter para fechar esta janela...")
